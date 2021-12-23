e607ed243a901c3b4527c433bb652a2452eaa2b5
Dov Moshe <dovbarak.moshe@live.biu.ac.il>
2021-12-23 22:02:48 +0200

    Update: Car with controller

---
+++ unity/DrivingTestAnimation/Assets/AdvanceCarController.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public struct Wheel
+{
+    public GameObject transform;
+    public WheelCollider collider;
+    public bool isFront;
+}
+
+public class AdvanceCarController : MonoBehaviour
+{
+
+    [SerializeField]
+    private float verticalInput;
+    [SerializeField]
+    private float horizontalInput;
+    [SerializeField]
+    private bool breakWheels;
+
+    [SerializeField]
+    private List<Wheel> wheels;
+
+    [SerializeField]
+    private float straight = 20000f;
+    [SerializeField]
+    private float maxTuen = 30f;
+
+
+    void Update()
+    {
+        GetInput();
+    }
+
+    void FixedUpdate()
+    {
+        UpdateMotor();
+        UpdateWheels();
+        UpdateSteering();
+    }
+
+    void GetInput()
+    {
+        verticalInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+        breakWheels = Input.GetKey("space");
+    }
+
+    void UpdateMotor()
+    {
+        foreach (Wheel wheel in wheels)
+        {
+            wheel.collider.motorTorque = straight * Time.deltaTime * verticalInput;
+        }
+    }
+
+    void UpdateSteering()
+    {
+        foreach (Wheel wheel in wheels)
+        {
+            if(wheel.isFront)
+            {
+                float angle = maxTuen * horizontalInput;
+                wheel.collider.steerAngle = Mathf.Lerp(wheel.collider.steerAngle, angle, 0.5f);
+            }
+
+        }
+    }
+
+    void UpdateWheels()
+    {
+        foreach(Wheel wheel in wheels)
+            {
+                Vector3 position;
+                Quaternion rotation;
+                wheel.collider.GetWorldPose(out position, out rotation);
+                wheel.transform.transform.position = position;
+                wheel.transform.transform.rotation = rotation;
+            }
+    }
+
+
+
+
+}
