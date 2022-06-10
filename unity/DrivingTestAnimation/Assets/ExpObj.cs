b68486671902e84167e482f76f63b1a94f347852
Dov Moshe <dovbarak.moshe@live.biu.ac.il>
2022-06-10 18:50:13 +0300

    Deleted files

--- unity/DrivingTestAnimation/Assets/ExpObj.cs
+++
@@ -1,8 +0,0 @@
-[System.Serializable]
-public class ExpObj
-{
-    public string title;
-    public string explanation;
-
-    public ExpObj(string title, string explanation) { this.title = title; this.explanation = explanation; }
-}
